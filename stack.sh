dad7368e304e60544329b83cfd23b66ea8e22143
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2020-08-26 01:06:38 +0200

    Update stack.sh

--- stack.sh
+++ stack.sh
@@ -4,10 +4,15 @@ set -eu
 
 cd lib/source
 
-arm-none-eabi-gcc -DNO_PTR_CALL -fdump-rtl-dfinish -fstack-usage \
-    -Os -fomit-frame-pointer -mthumb -mcpu=cortex-m0 \
-    -I ../include ecc.c ecc_dh.c ecc_dsa.c -c
+for CPU in m0 m4 a7; do
+    echo "*** $CPU ***"
+    arm-none-eabi-gcc -DNO_PTR_CALL -fdump-rtl-dfinish -fstack-usage \
+        -Os -fomit-frame-pointer -mthumb -mcpu=cortex-$CPU \
+        -I ../include ecc.c ecc_dh.c ecc_dsa.c -c
 
-python3 wcs.py | egrep 'uECC_(sign|verify|make_key|shared_secret)\>'
+    python3 wcs.py |
+        egrep 'uECC_(sign|verify|make_key|shared_secret)\>' |
+        sort
+done
 
 rm *.o *.su *.dfinish
