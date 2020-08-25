60cf8372a579b4d1437a9858a6b48a6064e39bde
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2020-08-25 23:10:46 +0200

    s/a5/a7/ in scripts

--- deps.sh
+++ deps.sh
@@ -6,7 +6,7 @@ set -eu
 
 TC_SYM_RE='uECC_|EccPoint_|XYcZ_|apply_z|bits2int|cond_set|curve_|double_|default_|entry_func|g_rng_|regularize_k|vli_|x_side_'
 
-for CPU in m0 m4 a5; do
+for CPU in m0 m4 a7; do
     printf "\n*** %s ***\n" $CPU
     arm-none-eabi-gcc -Os -mthumb -mcpu=cortex-$CPU entry.c \
         -I lib/include lib/source/ecc_platform_specific.c \
--- sizes.sh
+++ sizes.sh
@@ -5,7 +5,7 @@ set -eu
 cd lib/source
 rm -f ecc*.o
 
-for CPU in m0 m4 a5; do
+for CPU in m0 m4 a7; do
     printf "$CPU: "
     arm-none-eabi-gcc -Werror -Wall -Wextra -pedantic --std=c99 \
         -Os -fomit-frame-pointer -mthumb -mcpu=cortex-$CPU \
