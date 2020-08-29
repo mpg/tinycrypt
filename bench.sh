be5fb0d749300b329b7e19c422bcbbb60aeba7f1
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2020-08-29 11:21:50 +0200

    Use -march=native for on-host benchmark

--- bench.sh
+++ bench.sh
@@ -3,10 +3,12 @@
 set -eu
 
 gcc --std=c99 -Werror -Wall -Wextra -pedantic \
-    -Os -I lib/include lib/source/ecc_platform_specific.c \
+    -Os -march=native \
+    -I lib/include lib/source/ecc_platform_specific.c \
     lib/source/ecc.c lib/source/ecc_dh.c lib/source/ecc_dsa.c \
     benchmark.c -o benchmark
 
+echo "Build complete, running benchmark"
 ./benchmark
 
 rm benchmark
